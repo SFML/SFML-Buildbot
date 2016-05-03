1bd278ed99e28d9b023338b031ac81d728de3b34
Marco Antognini <antognini.marco@gmail.com>
2016-05-03 22:56:42 +0200

    Fix OS X configuration (#3)

--- buildfactories.py
+++ buildfactories.py
@@ -18,17 +18,19 @@ def get_cmake_step(link, type, osxExtra = []):
         shared_libs += '-DBUILD_SHARED_LIBS=TRUE'
 
     build_frameworks = ''
+    build_target = ''
+    build_sdk = ''
     suffix = ''
 
     if 'frameworks' in osxExtra:
         build_frameworks += '-DSFML_BUILD_FRAMEWORKS=TRUE'
-        suffix = [link, type, 'frameworks'];
+        suffix = [link, type, 'frameworks']
     else:
         build_frameworks += '-DSFML_BUILD_FRAMEWORKS=FALSE'
-        suffix = [link, type];
+        suffix = [link, type]
 
     if 'oldSDK' in osxExtra:
-        build_sdk += '-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7'
+        build_target += '-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7'
         build_sdk += '-DCMAKE_OSX_SYSROOT=/Developer/SDKs/MacOSX10.7.sdk'
         suffix.append('10.7')
 
@@ -37,9 +39,9 @@ def get_cmake_step(link, type, osxExtra = []):
         description = ['configuring'],
         descriptionSuffix = suffix,
         descriptionDone = ['configure'],
-        doStepIf = lambda step : ((not osxExtra) or ('osx' in step.build.getProperty('buildername')))) and (link != 'static' or not ('osx' in step.build.getProperty('buildername')))
+        doStepIf = lambda step : ((not osxExtra) or ('osx' in step.build.getProperty('buildername'))) and (link != 'static' or not ('osx' in step.build.getProperty('buildername'))),
         hideStepIf = lambda results, step : results == SKIPPED,
-        command = ['cmake', '-G', Interpolate('%(prop:generator)s'), '-DSFML_BUILD_EXAMPLES=TRUE', Interpolate('-DCMAKE_INSTALL_PREFIX=%(prop:workdir)s/install'), Interpolate('-DCMAKE_INSTALL_FRAMEWORK_PREFIX=%(prop:workdir)s/install/Library/Frameworks'), build_type, shared_libs, build_frameworks, build_sdk, '.'],
+        command = ['cmake', '-G', Interpolate('%(prop:generator)s'), '-DSFML_BUILD_EXAMPLES=TRUE', Interpolate('-DCMAKE_INSTALL_PREFIX=%(prop:workdir)s/install'), Interpolate('-DCMAKE_INSTALL_FRAMEWORK_PREFIX=%(prop:workdir)s/install/Library/Frameworks'), build_type, shared_libs, build_frameworks, build_sdk, build_target, '.'],
         env = {
             'PATH' : Interpolate('%(prop:toolchain_path)s%(prop:PATH)s'),
             'INCLUDE' : Interpolate('%(prop:vc_include)s'),
@@ -57,22 +59,24 @@ def get_build_step(link, type, osxExtra = []):
     from buildbot.status.results import SKIPPED
 
     suffix = ''
+    target = 'install'
 
     if 'frameworks' in osxExtra:
-        suffix = [link, type, 'frameworks'];
+        suffix = [link, type, 'frameworks']
     else:
-        suffix = [link, type];
+        suffix = [link, type]
 
     if 'oldSDK' in osxExtra:
         suffix.append('10.7')
+        target = 'all'
 
     return Compile(
         description = ['building'],
         descriptionSuffix = suffix,
         descriptionDone = ['build'],
-        doStepIf = lambda step : ((not osxExtra) or ('osx' in step.build.getProperty('buildername')))) and (link != 'static' or not ('osx' in step.build.getProperty('buildername')))
+        doStepIf = lambda step : ((not osxExtra) or ('osx' in step.build.getProperty('buildername'))) and (link != 'static' or not ('osx' in step.build.getProperty('buildername'))),
         hideStepIf = lambda results, step : results == SKIPPED,
-        command = [Interpolate('%(prop:maker)s'), '-j', Interpolate('%(prop:parallel)s'), 'install'],
+        command = [Interpolate('%(prop:maker)s'), '-j', Interpolate('%(prop:parallel)s'), target],
         env = {
             'PATH' : Interpolate('%(prop:toolchain_path)s%(prop:PATH)s'),
             'INCLUDE' : Interpolate('%(prop:vc_include)s'),
@@ -116,7 +120,6 @@ def get_build_factory():
             },
             logEnviron = False
         ),
-        get_cmake_step('dynamic', 'debug', ['oldSDK']), # do it first so that files are overriden by other steps
         get_cmake_step('dynamic', 'debug'),
         get_build_step('dynamic', 'debug'),
         get_cmake_step('static', 'debug'),
@@ -127,6 +130,8 @@ def get_build_factory():
         get_build_step('static', 'release'),
         get_cmake_step('dynamic', 'release', ['frameworks']),
         get_build_step('dynamic', 'release', ['frameworks']),
+        get_cmake_step('dynamic', 'debug', ['oldSDK']),
+        get_build_step('dynamic', 'debug', ['oldSDK']),
         DirectoryUpload(
             description = ['uploading'],
             descriptionSuffix = ['artifact'],
