68448d4c1cd7ead3d42d8c81557ef5f3c2e0b968
binary1248 <binary1248@hotmail.com>
2018-12-04 01:05:18 +0100

    Adjusted issue_comment_hook.php to use GitHub's pull request head instead of an origin repository and branch name.

--- public_html/issue_comment_hook.php
+++ public_html/issue_comment_hook.php
@@ -29,26 +29,6 @@ function getMembers()
 	return $members;
 }
 
-function getBuildParameters($prId)
-{
-	global $buildBotToken;
-
-	$buildBotClient = new \Github\Client();
-	$buildBotClient->authenticate($buildBotToken, null, \Github\Client::AUTH_HTTP_TOKEN);
-
-	try
-	{
-		$pullRequest = $buildBotClient->api("pull_request")->show("SFML", "SFML", $prId);
-
-		return array("repository" => $pullRequest["head"]["repo"]["full_name"], "branch" => $pullRequest["head"]["ref"]);
-	}
-	catch(Exception $e)
-	{
-	}
-
-	return array("repository" => "", "branch" => "");
-}
-
 function getBuildRequest($data)
 {
 	if((!array_key_exists("action", $data)) || ($data["action"] != "created"))
@@ -84,40 +64,36 @@ function main()
 
 	if($request["id"] != 0)
 	{
-		$buildParameters = getBuildParameters($request["id"]);
+		$user = $request["user"];
+		$branch = "refs/pull/" . $request["id"] . "/head";
+		$repository = "SFML/SFML";
+		$comment = "Starting build requested by $user for pull request #" . $request["id"] . " on $repository.";
 
-		if(!empty($buildParameters["repository"]) && !empty($buildParameters["branch"]))
-		{
-			$user = $request["user"];
-			$branch = $buildParameters["branch"];
-			$repository = $buildParameters["repository"];
-			$comment = "Starting build requested by $user for $branch on $repository.";
+		$data = array(
+			"repository" => "https://github.com/$repository.git",
+			"project" => "SFML",
+			"branch" => $branch,
+			"comments" => $comment,
+			"author" => $user
+		);
 
-			$data = array(
-				"repository" => "https://github.com/$repository.git",
-				"project" => "SFML",
-				"branch" => $branch,
-				"comments" => $comment,
-				"author" => $user
-			);
+		$options = array(
+			"http" => array(
+				"header"  => "Content-type: application/x-www-form-urlencoded\r\n",
+				"method"  => "POST",
+				"content" => http_build_query($data)
+			)
+		);
 
-			$options = array(
-				"http" => array(
-					"header"  => "Content-type: application/x-www-form-urlencoded\r\n",
-					"method"  => "POST",
-					"content" => http_build_query($data)
-				)
-			);
+		$context = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
 
-			$context = stream_context_create($options);
-			$result = file_get_contents($url, false, $context);
-
-			print("<html><head><title>Build started.</title></head><body>Build started.</body></html>");
-			return;
-		}
+		print("<html><head><title>Build started.</title></head><body>Build started.</body></html>");
 	}
-
-	print("<html><head><title>Build not started.</title></head><body>Build not started.</body></html>");
+	else
+	{
+		print("<html><head><title>Build not started.</title></head><body>Build not started. (id = 0)</body></html>");
+	}
 }
 
 main();
