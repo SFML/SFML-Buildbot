aed853a86555aefac1466c2c8374daba6e3ab136
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2020-12-01 01:07:10 +0100

    Add Raspbian armhf worker

--- buildslaves.py
+++ buildslaves.py
@@ -11,6 +11,7 @@ def get_slaves():
     return [
         Worker('unassigned', private.slave_passwords['unassigned'], max_builds = 1, properties = {'parallel' : 0}),
         Worker('expl0it3r-windows', private.slave_passwords['expl0it3r-windows'], properties = {'parallel' : 1}),
+        Worker('expl0it3r-raspbian-armhf', private.slave_passwords['expl0it3r-raspbian-armhf'], properties = {'parallel' : 3}),
         Worker('macos', private.slave_passwords['macos'], max_builds = 1, properties = {'parallel' : 3}),
         Worker('binary1248-debian-64', private.slave_passwords['binary1248-debian-64'], max_builds = 1, properties = {'parallel' : 5}),
         Worker('binary1248-freebsd-64', private.slave_passwords['binary1248-freebsd-64'], max_builds = 1, properties = {'parallel' : 1}),
