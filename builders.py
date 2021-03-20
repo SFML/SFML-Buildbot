SFML/SFML-Buildbot

commit e20f520c414522cb2a0166087bdee88dfb0dfd40

Add macOS arm64 builder

builders.py
@@ -20,7 +20,7 @@ def workerPriority(builder, workers, requests):
 def get_builder_names():
     return builder_names
 
-def make_builder(builder_name, workers, generator, toolchain_path, vc_target, artifact, run_tests):
+def make_builder(builder_name, workers, generator, toolchain_path, architecture, artifact, run_tests):
     from buildbot.config import BuilderConfig
     import buildfactories
 
@@ -51,7 +51,7 @@ def make_builder(builder_name, workers, generator, toolchain_path, vc_target, ar
             'generator' : generator,
             'makefile' : makefile,
             'toolchain_path' : toolchain_path,
-            'vc_target' : vc_target,
+            'architecture' : architecture,
             'artifact' : artifact,
             'artifact_extension' : artifact_extension,
             'archive_command' : archive_command,
@@ -79,6 +79,7 @@ def get_builders():
         make_builder('android-armeabi-v7a-api13', ['binary1248-debian-64'], 'Unix Makefiles', '', '', True, False),
         make_builder('static-analysis', ['binary1248-debian-64'], 'Unix Makefiles', '', '', False, False),
         make_builder('freebsd-gcc-64', ['binary1248-freebsd-64'], 'Unix Makefiles', '', '', False, True),
-        make_builder('osx-clang', ['macos'], 'Unix Makefiles', '', '', True, True),
-        make_builder('ios-clang', ['macos'], 'Xcode', '', '', True, False)
+        make_builder('macos-clang-64', ['macos-64'], 'Unix Makefiles', '', 'x86_64', True, True),
+        make_builder('ios-clang-64', ['macos-64'], 'Xcode', '', '', True, False)
+        make_builder('macos-clang-arm64', ['macos-arm64'], 'Unix Makefiles', '', 'arm64', True, True),
     ]

buildfactories.py
@@ -35,6 +35,7 @@ def get_cmake_step(link, type, options = []):
     install_prefix = Interpolate('-DCMAKE_INSTALL_PREFIX=%(prop:builddir)s/install')
     frameworks_install_directory = Interpolate('')
     misc_install_directory = Interpolate('')
+    macos_architecture = ''
     ios_platform = ''
     suffix = ''
 
@@ -60,12 +61,13 @@ def get_cmake_step(link, type, options = []):
         build_sdk += '-DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/iOS.toolchain.cmake'
         ios_platform = '-DIOS_PLATFORM=SIMULATOR'
 
-    if 'osx' in options:
+    if 'macos' in options:
         install_prefix = Interpolate('-DCMAKE_INSTALL_PREFIX=%(prop:builddir)s/install/Library/Frameworks')
         frameworks_install_directory = Interpolate('-DSFML_DEPENDENCIES_INSTALL_PREFIX=%(prop:builddir)s/install/Library/Frameworks')
         misc_install_directory = Interpolate('-DSFML_MISC_INSTALL_PREFIX=%(prop:builddir)s/install/share/SFML')
+        macos_architecture = Interpolate('-DCMAKE_OSX_ARCHITECTURES=%(prop:architecture)')
 
-    configure_command = ['cmake', '-G', Interpolate('%(prop:generator)s'), '-DSFML_BUILD_EXAMPLES=TRUE', Interpolate('-DSFML_BUILD_TEST_SUITE=%(prop:run_tests)s'), ios_platform, install_prefix, frameworks_install_directory, misc_install_directory, build_type, shared_libs, build_frameworks, build_sdk, build_ndk, build_stl_type, build_target, '..']
+    configure_command = ['cmake', '-G', Interpolate('%(prop:generator)s'), '-DSFML_BUILD_EXAMPLES=TRUE', Interpolate('-DSFML_BUILD_TEST_SUITE=%(prop:run_tests)s'), macos_architecture, ios_platform, install_prefix, frameworks_install_directory, misc_install_directory, build_type, shared_libs, build_frameworks, build_sdk, build_ndk, build_stl_type, build_target, '..']
 
     if 'scan-build' in options:
         configure_command.insert(0, 'scan-build')
@@ -75,7 +77,7 @@ def get_cmake_step(link, type, options = []):
         description = ['configuring'],
         descriptionSuffix = suffix,
         descriptionDone = ['configure'],
-        doStepIf = lambda step : ('scan-build' in options) or ('android' in options) or ('ios' in options) or (((not options) or ('osx' in step.build.getProperty('buildername'))) and (link != 'static' or not ('osx' in step.build.getProperty('buildername')))),
+        doStepIf = lambda step : ('scan-build' in options) or ('android' in options) or ('ios' in options) or (((not options) or ('macos' in step.build.getProperty('buildername'))) and (link != 'static' or not ('macos' in step.build.getProperty('buildername')))),
         hideStepIf = skipped,
         workdir = Interpolate('%(prop:builddir)s/build/build'),
         command = configure_command,
@@ -128,7 +130,7 @@ def get_build_step(link, type, options = []):
         description = ['building'],
         descriptionSuffix = suffix,
         descriptionDone = ['build'],
-        doStepIf = lambda step : ('scan-build' in options) or ('android' in options) or ('ios' in options) or (((not options) or ('osx' in step.build.getProperty('buildername'))) and (link != 'static' or not ('osx' in step.build.getProperty('buildername')))),
+        doStepIf = lambda step : ('scan-build' in options) or ('android' in options) or ('ios' in options) or (((not options) or ('macos' in step.build.getProperty('buildername'))) and (link != 'static' or not ('macos' in step.build.getProperty('buildername')))),
         hideStepIf = skipped,
         workdir = Interpolate('%(prop:builddir)s/build/build'),
         command = Interpolate('%(kw:command)s %(prop:makefile:#?| -- -j %(prop:parallel)s|)s', command = build_command),
@@ -177,7 +179,7 @@ def get_vs_env_step():
     from buildbot.steps.shell import SetPropertyFromCommand
     from buildbot.process.properties import Interpolate
 
-    return [SetPropertyFromCommand(env = {'PATH' : Interpolate('%(prop:toolchain_path)s%(prop:PATH)s')}, command = Interpolate('vcvarsall.bat %(prop:vc_target)s > nul && set'), extract_fn = extract_vs_paths)]
+    return [SetPropertyFromCommand(env = {'PATH' : Interpolate('%(prop:toolchain_path)s%(prop:PATH)s')}, command = Interpolate('vcvarsall.bat %(prop:architecture)s > nul && set'), extract_fn = extract_vs_paths)]
 
 def get_clone_step():
     from buildbot.steps.source.git import Git
@@ -216,7 +218,7 @@ def get_configuration_build_steps(link, type, options = []):
             description = ['preparing build directory'],
             descriptionDone = ['create build directory'],
             dir = Interpolate('%(prop:builddir)s/build/build'),
-            doStepIf = lambda step : (bool(options) and ('osx' in step.build.getProperty('buildername'))),
+            doStepIf = lambda step : (bool(options) and ('macos' in step.build.getProperty('buildername'))),
             hideStepIf = skipped_or_success
         ),
         get_cmake_step(link, type, options),
@@ -226,7 +228,7 @@ def get_configuration_build_steps(link, type, options = []):
             description = ['removing build directory'],
             descriptionDone = ['remove build directory'],
             dir = Interpolate('%(prop:builddir)s/build/build'),
-            doStepIf = lambda step : (bool(options) and ('osx' in step.build.getProperty('buildername'))),
+            doStepIf = lambda step : (bool(options) and ('macos' in step.build.getProperty('buildername'))),
             hideStepIf = skipped_or_success
         )
     ]
@@ -396,14 +398,14 @@ def get_build_factory(builder_name):
        # Only static on ios
         steps.extend(get_configuration_build_steps('static', 'debug', ['ios']))
         steps.extend(get_configuration_build_steps('static', 'release', ['ios']))
-    elif('osx' in builder_name):
-        steps.extend(get_configuration_build_steps('dynamic', 'debug', ['osx']))
-        steps.extend(get_configuration_build_steps('static', 'debug', ['osx']))
-        steps.extend(get_configuration_build_steps('dynamic', 'release', ['osx']))
-        steps.extend(get_configuration_build_steps('static', 'release', ['osx']))
+    elif('macos' in builder_name):
+        steps.extend(get_configuration_build_steps('dynamic', 'debug', ['macos']))
+        steps.extend(get_configuration_build_steps('static', 'debug', ['macos']))
+        steps.extend(get_configuration_build_steps('dynamic', 'release', ['macos']))
+        steps.extend(get_configuration_build_steps('static', 'release', ['macos']))
 
-        steps.extend(get_configuration_build_steps('dynamic', 'release', ['osx', 'frameworks']))
-        steps.extend(get_configuration_build_steps('dynamic', 'debug', ['osx', 'newSDK']))
+        steps.extend(get_configuration_build_steps('dynamic', 'release', ['macos', 'frameworks']))
+        steps.extend(get_configuration_build_steps('dynamic', 'debug', ['macos', 'newSDK']))
 
         steps.extend(get_artifact_step())
     else:

buildslaves.py
@@ -12,7 +12,8 @@ def get_slaves():
         Worker('unassigned', private.slave_passwords['unassigned'], max_builds = 1, properties = {'parallel' : 0}),
         Worker('expl0it3r-windows', private.slave_passwords['expl0it3r-windows'], properties = {'parallel' : 1}),
         Worker('expl0it3r-raspbian-armhf', private.slave_passwords['expl0it3r-raspbian-armhf'], properties = {'parallel' : 3}),
-        Worker('macos', private.slave_passwords['macos'], max_builds = 1, properties = {'parallel' : 3}),
+        Worker('macos-64', private.slave_passwords['macos-64'], max_builds = 1, properties = {'parallel' : 3}),
+        Worker('macos-arm64', private.slave_passwords['macos-arm64'], max_builds = 1, properties = {'parallel' : 3}),
         Worker('binary1248-debian-64', private.slave_passwords['binary1248-debian-64'], max_builds = 1, properties = {'parallel' : 5}),
         Worker('binary1248-freebsd-64', private.slave_passwords['binary1248-freebsd-64'], max_builds = 1, properties = {'parallel' : 1}),
         Worker('binary1248-windows', private.slave_passwords['binary1248-windows'], max_builds = 1, properties = {'parallel' : 8})

private.py.in
@@ -6,7 +6,8 @@ slave_passwords = dict([
 	('unassigned',               '<password redacted>'),
 	('expl0it3r-windows',        '<password redacted>'),
 	('expl0it3r-raspbian-armhf', '<password redacted>'),
-	('macos',                    '<password redacted>'),
+	('macos-64',                 '<password redacted>'),
+	('macos-arm64',              '<password redacted>'),
 	('master',                   '<password redacted>'),
 	('master-debian-64',         '<password redacted>'),
 	('master-ubuntu-64',         '<password redacted>'),

commit 7146d16470b0a94a07602ec2fe465ee1bb78da5a

Fixed syntax errors introduced in e20f520c414522cb2a0166087bdee88dfb0dfd40.

builders.py
@@ -80,6 +80,6 @@ def get_builders():
         make_builder('static-analysis', ['binary1248-debian-64'], 'Unix Makefiles', '', '', False, False),
         make_builder('freebsd-gcc-64', ['binary1248-freebsd-64'], 'Unix Makefiles', '', '', False, True),
         make_builder('macos-clang-64', ['macos-64'], 'Unix Makefiles', '', 'x86_64', True, True),
-        make_builder('ios-clang-64', ['macos-64'], 'Xcode', '', '', True, False)
+        make_builder('ios-clang-64', ['macos-64'], 'Xcode', '', '', True, False),
         make_builder('macos-clang-arm64', ['macos-arm64'], 'Unix Makefiles', '', 'arm64', True, True),
     ]

buildfactories.py
@@ -35,7 +35,7 @@ def get_cmake_step(link, type, options = []):
     install_prefix = Interpolate('-DCMAKE_INSTALL_PREFIX=%(prop:builddir)s/install')
     frameworks_install_directory = Interpolate('')
     misc_install_directory = Interpolate('')
-    macos_architecture = ''
+    macos_architecture = Interpolate('')
     ios_platform = ''
     suffix = ''
 
@@ -65,7 +65,7 @@ def get_cmake_step(link, type, options = []):
         install_prefix = Interpolate('-DCMAKE_INSTALL_PREFIX=%(prop:builddir)s/install/Library/Frameworks')
         frameworks_install_directory = Interpolate('-DSFML_DEPENDENCIES_INSTALL_PREFIX=%(prop:builddir)s/install/Library/Frameworks')
         misc_install_directory = Interpolate('-DSFML_MISC_INSTALL_PREFIX=%(prop:builddir)s/install/share/SFML')
-        macos_architecture = Interpolate('-DCMAKE_OSX_ARCHITECTURES=%(prop:architecture)')
+        macos_architecture = Interpolate('-DCMAKE_OSX_ARCHITECTURES=%(prop:architecture)s')
 
     configure_command = ['cmake', '-G', Interpolate('%(prop:generator)s'), '-DSFML_BUILD_EXAMPLES=TRUE', Interpolate('-DSFML_BUILD_TEST_SUITE=%(prop:run_tests)s'), macos_architecture, ios_platform, install_prefix, frameworks_install_directory, misc_install_directory, build_type, shared_libs, build_frameworks, build_sdk, build_ndk, build_stl_type, build_target, '..']
 