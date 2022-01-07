30a1d77020b286539ba3bc3dff8f0652491bbe65
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2022-01-07 21:30:36 +0100

    Use WinLibs MinGW with GCC 11 support

--- paths.py
+++ paths.py
@@ -13,6 +13,6 @@ vc17path = (
     'C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/VC/Auxiliary/Build;'
 )
 
-gcc810mingw32path = 'C:/Dev/MinGW32-Win32Dwarf810r0/bin/;'
+gcc1120mingw32path = 'C:/Dev/MinGW32-WinLibs-PosixDwarf1120r3/bin/;'
 
-gcc810mingw64path = 'C:/Dev/MinGW64-Win32SEH810r0/bin/;'
+gcc1120mingw64path = 'C:/Dev/MinGW64-WinLibs-PosixSEH1120r3/bin/;'
