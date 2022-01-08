1814e23c37d622c05e504f88396b16354746937a
binary1248 <binary1248@hotmail.com>
2022-01-08 13:33:56 +0100

    Increase parallel building on Debian from 2 to 3.

--- buildslaves.py
+++ buildslaves.py
@@ -14,7 +14,7 @@ def get_slaves():
         Worker('expl0it3r-raspbian-armhf', private.slave_passwords['expl0it3r-raspbian-armhf'], properties = {'parallel' : 3}),
         Worker('macos-64', private.slave_passwords['macos-64'], max_builds = 1, properties = {'parallel' : 3}),
         Worker('macos-arm64', private.slave_passwords['macos-arm64'], max_builds = 1, properties = {'parallel' : 3}),
-        Worker('binary1248-debian', private.slave_passwords['binary1248-debian'], max_builds = 1, properties = {'parallel' : 2}),
+        Worker('binary1248-debian', private.slave_passwords['binary1248-debian'], max_builds = 1, properties = {'parallel' : 3}),
         Worker('binary1248-freebsd-64', private.slave_passwords['binary1248-freebsd-64'], max_builds = 1, properties = {'parallel' : 1}),
         Worker('binary1248-windows', private.slave_passwords['binary1248-windows'], max_builds = 1, properties = {'parallel' : 8}),
         Worker('binary1248-android', private.slave_passwords['binary1248-android'], max_builds = 1, properties = {'parallel' : 1})
