e7275bafface268320a5c2d7205425e49604e5e9
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2024-12-20 22:00:37 +0100

    Update MinGW to GCC 14.2.0

--- paths.py
+++ paths.py
@@ -8,6 +8,6 @@ vc17path = (
     'C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/VC/Auxiliary/Build;'
 )
 
-gcc1220mingw32path = 'C:/Dev/MinGW32-WinLibs-PosixDwarf1220r4/bin/;'
+gcc1420mingw32path = 'C:/Dev/MinGW32-WinLibs-PosixDwarf1420r2/bin/;'
 
-gcc1220mingw64path = 'C:/Dev/MinGW64-WinLibs-PosixSEH1220r4/bin/;'
+gcc1420mingw64path = 'C:/Dev/MinGW64-WinLibs-PosixSEH1420r2/bin/;'
