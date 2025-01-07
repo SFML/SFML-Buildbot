fc4490727e218bfe227b18feae15351c2da72098
binary1248 <binary1248@hotmail.com>
2025-01-07 14:29:34 +0100

    Update master to Buildbot 4.0.4.

--- docker/build.sh
+++ docker/build.sh
@@ -5,3 +5,4 @@ DOCKER_BUILDKIT=1
 docker build -t buildbot-worker-debian - < debian/Dockerfile
 docker build -t buildbot-worker-android - < android/Dockerfile
 docker build --build-arg COVERITY_TOKEN=$COVERITY_TOKEN --build-arg SONAR_TOKEN=$SONAR_TOKEN -t buildbot-worker-coverity - < coverity/Dockerfile
+docker build -t buildbot-master - < master/Dockerfile
