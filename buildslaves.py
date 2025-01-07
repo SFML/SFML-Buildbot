c7273d80b0c041c8c330b13aa6c5ae137e581994
binary1248 <binary1248@hotmail.com>
2025-01-07 14:27:23 +0100

    Tweak worker properties.

--- buildslaves.py
+++ buildslaves.py
@@ -15,14 +15,14 @@ def get_slaves():
         Worker('macos-64', private.slave_passwords['macos-64'], max_builds = 1, properties = {'parallel' : 3}),
         Worker('macos-arm64', private.slave_passwords['macos-arm64'], max_builds = 1, properties = {'parallel' : 3}),
 
-        Worker('expl0it3r-windows', private.slave_passwords['expl0it3r-windows'], max_builds = 1, properties = {'parallel' : 4}),
-        Worker('expl0it3r-raspbian-armhf', private.slave_passwords['expl0it3r-raspbian-armhf'], max_builds = 1, properties = {'parallel' : 3}),
+        Worker('expl0it3r-windows', private.slave_passwords['expl0it3r-windows'], max_builds = 1, properties = {'parallel' : 3}),
+        Worker('expl0it3r-raspbian-armhf', private.slave_passwords['expl0it3r-raspbian-armhf'], max_builds = 1, properties = {'parallel' : 2}),
 
         Worker('binary1248-freebsd-64', private.slave_passwords['binary1248-freebsd-64'], max_builds = 1, properties = {'parallel' : 1}),
-        Worker('binary1248-windows', private.slave_passwords['binary1248-windows'], max_builds = 1, properties = {'parallel' : 4}),
+        Worker('binary1248-windows', private.slave_passwords['binary1248-windows'], max_builds = 1, properties = {'parallel' : 3}),
         Worker('binary1248-raspbian-armhf', private.slave_passwords['binary1248-raspbian-armhf'], max_builds = 1, properties = {'parallel' : 2}),
 
-        worker.DockerLatentWorker('binary1248-debian',   private.slave_passwords['binary1248-debian'],   docker_host = 'unix:///var/run/docker.sock', image = 'buildbot-worker-debian',   hostconfig = {'network_mode' : 'container:sfml-ci-network'}, followStartupLogs = True, masterFQDN = 'ci.sfml-dev.org', build_wait_timeout = 0, max_builds = 1, properties = {'parallel' : 2}),
-        worker.DockerLatentWorker('binary1248-android',  private.slave_passwords['binary1248-android'],  docker_host = 'unix:///var/run/docker.sock', image = 'buildbot-worker-android',  hostconfig = {'network_mode' : 'container:sfml-ci-network'}, followStartupLogs = True, masterFQDN = 'ci.sfml-dev.org', build_wait_timeout = 0, max_builds = 1, properties = {'parallel' : 1}),
-        worker.DockerLatentWorker('binary1248-coverity', private.slave_passwords['binary1248-coverity'], docker_host = 'unix:///var/run/docker.sock', image = 'buildbot-worker-coverity', hostconfig = {'network_mode' : 'container:sfml-ci-network'}, followStartupLogs = True, masterFQDN = 'ci.sfml-dev.org', build_wait_timeout = 0, max_builds = 1, properties = {'parallel' : 1})
+        worker.DockerLatentWorker('binary1248-debian',   private.slave_passwords['binary1248-debian'],   docker_host = 'unix:///var/run/docker.sock', image = 'buildbot-worker-debian',   hostconfig = {'network_mode' : 'container:sfml-ci-network'}, masterFQDN = 'ci.sfml-dev.org', build_wait_timeout = 0, max_builds = 1, properties = {'parallel' : 1}),
+        worker.DockerLatentWorker('binary1248-android',  private.slave_passwords['binary1248-android'],  docker_host = 'unix:///var/run/docker.sock', image = 'buildbot-worker-android',  hostconfig = {'network_mode' : 'container:sfml-ci-network'}, masterFQDN = 'ci.sfml-dev.org', build_wait_timeout = 0, max_builds = 1, properties = {'parallel' : 1}),
+        worker.DockerLatentWorker('binary1248-coverity', private.slave_passwords['binary1248-coverity'], docker_host = 'unix:///var/run/docker.sock', image = 'buildbot-worker-coverity', hostconfig = {'network_mode' : 'container:sfml-ci-network'}, masterFQDN = 'ci.sfml-dev.org', build_wait_timeout = 0, max_builds = 1, properties = {'parallel' : 1})
     ]
